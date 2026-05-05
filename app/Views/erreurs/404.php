859dc148942d949c3974f2b63d7854d2dcde3c0c
basma rhilane <basma.rhilane@esme.fr>
2026-05-05 12:09:16 +0200

    Test-404

--- app/Views/erreurs/404.php
+++ app/Views/erreurs/404.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 — Page introuvable | ABS</title>
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
+
 <body>
     <main class="site-main">
         <div class="conteneur" style="text-align:center; padding: 4rem 1rem;">
             <h1>404</h1>
-            <p>Désolé, cette page n’existe pas ou a été déplacée.</p>
+            <p>AHHH Désolé, cette page n’existe pas ou a été déplacée.</p>
             <p><a class="btn" href="/">Retour à l’accueil</a></p>
         </div>
     </main>
 </body>
+
 </html>
